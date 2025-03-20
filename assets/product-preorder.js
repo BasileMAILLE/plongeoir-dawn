42fd8ccff134e8fd7b56c2ac94d48cae7eb5e901
Basile MAILLE <bmaille@ecocealafabrique.com>
2025-03-20 16:35:11 +0100

    feat(product): change add to cart button when preorder

--- assets/product-preorder.js
+++ assets/product-preorder.js
@@ -1,19 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("product-preorder.js loaded");
-  console.log(variants_metafields)
+  console.info("product-preorder.js loaded");
 
   const variantSelectors = document.querySelector("[name='id']");
   const preorderMessage = document.getElementById("preorder-message");
+  const buyButton = document.querySelector("[name='add'] span");
+  const preOrderButtonMessage = window.variantStrings.preorderButton;
 
   if (!variantSelectors) return;
 
   variantSelectors.addEventListener("change", function (e) {
+    console.log("variant changed");
     if (variants_metafields[e.target.value]) {
       let selectedVariant = variants_metafields[e.target.value];
       let availabilityDate = selectedVariant.preorder.availability_message;
 
       preorderMessage.innerText = availabilityDate;
       preorderMessage.style.display = "block";
+      console.log(buyButton.innerText)
+      buyButton.innerText = preOrderButtonMessage;
+      console.log(buyButton.innerText)
     } else {
       preorderMessage.style.display = "none";
     }
