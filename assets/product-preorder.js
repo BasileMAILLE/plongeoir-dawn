3ae9acf1cc5805d50167bb6c9d22792b92c0c589
Basile MAILLE <bmaille@ecocealafabrique.com>
2025-03-21 15:44:39 +0100

    feat(all): migrate preorder displays to line item properties - show in checkout

--- assets/product-preorder.js
+++ assets/product-preorder.js
@@ -1,14 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.info("product-preorder.js loaded");
-
   const variantSelectors = document.querySelector("[name='id']");
   const preorderMessage = document.getElementById("preorder-message");
   const buyButton = document.querySelector("[name='add'] span");
   const preOrderButtonMessage = window.variantStrings.preorderButton;
-
+  const preorderInput = document.getElementById("preorder-date-input");
+  const preorderLabelInput = document.getElementById("preorder-date-input-label");
+  
   if (!variantSelectors) return;
+  updatePreorderDate(variantSelectors.value);
+  console.log(preorderInput, preorderLabelInput)
+
+  function updatePreorderDate(variantId) {
+    const date = window.preorder_availability_date[variantId];
+    preorderInput.value = date || "";
+    preorderLabelInput.value = window.preorder_availability_message[variantId] || "";
+  }
+
 
   variantSelectors.addEventListener("change", function (e) {
+    updatePreorderDate(e.target.value);
     if (window.preorder_availability_message[e.target.value]) {
       let availabilityDate = window.preorder_availability_message[e.target.value];
       preorderMessage.innerText = availabilityDate;
